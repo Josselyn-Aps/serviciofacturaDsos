7d192f51c56cdd7b9eb1580fc2169a9ba23ed568
testusuario <yosexd21@gmail.com>
2022-06-04 16:51:16 -0500

    controller

--- src/main/java/adu/mx/tecnm/oaxaca/serviciofacturaDsos/controller/FacturaController.java
+++ src/main/java/adu/mx/tecnm/oaxaca/serviciofacturaDsos/controller/FacturaController.java
@@ -4,7 +4,17 @@
  */
 package adu.mx.tecnm.oaxaca.serviciofacturaDsos.controller;
 
+import adu.mx.tecnm.oaxaca.serviciofacturaDsos.model.FacturaModel;
+import adu.mx.tecnm.oaxaca.serviciofacturaDsos.service.FacturaService;
+import adu.mx.tecnm.oaxaca.serviciofacturaDsos.utils.CustomResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +26,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/factura")
 public class FacturaController {
+@Autowired
+    private FacturaService facturaService;
+    @PostMapping("/")
+    public CustomResponse resgistrarFactura(@RequestBody FacturaModel factura){
+        CustomResponse customResponse = new CustomResponse();
+        facturaService.registrarFactura(factura);
+        return customResponse;
+    }
     
+    @GetMapping("/")
+    public CustomResponse getFacturas(){
+       CustomResponse customResponse = new CustomResponse();
+       customResponse.setData(facturaService.getFacturas());
+       return customResponse;
+    }
+    
+    @GetMapping("/{folio}")
+    public CustomResponse getVenta(@PathVariable int folio){
+       CustomResponse customResponse = new CustomResponse(); 
+       customResponse.setData(facturaService.getFactura(folio));
+       return customResponse;
+    }
+    
+    @PutMapping("/{folio}")
+    public CustomResponse updateFactura(@RequestBody FacturaModel factura, @PathVariable Integer folio){
+        CustomResponse customResponse = new CustomResponse();
+        facturaService.updateFactura(factura, folio);
+        return customResponse;
+    }
+    
+    @DeleteMapping("/{folio}")
+    public CustomResponse deleteFactura(@PathVariable Integer folio){
+        CustomResponse customResponse = new CustomResponse();
+        facturaService.deleteFactura(folio);
+         return customResponse;
+    }
 }
